4ad300949e5f4e34fd8b4801ca2ad9f53209a9fe
bmeism <news.manuelsworld@gmail.com>
2015-06-18 17:20:10 +0200

    Added location

---
+++ app/src/main/java/ch/post/education/apps/rainman/Model/Location.java
@@ -0,0 +1,41 @@
+package ch.post.education.apps.rainman.Model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Location {
+
+    private int id;
+    private String name;
+    private Coordinates coordinates;
+    private String country;
+    private int population;
+
+    public Location(JSONObject object) throws JSONException{
+        id = object.getInt("id");
+        name = object.getString("name");
+        coordinates = new Coordinates(object.getJSONObject("coord"));
+        country = object.getString("country");
+        population = object.getInt("population");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+}
