16cc782bc6896f0b497e26e64e2e3eae95fc414b
bmeism <news.manuelsworld@gmail.com>
2015-06-18 17:19:35 +0200

    Added wind

---
+++ app/src/main/java/ch/post/education/apps/rainman/Model/Wind.java
@@ -0,0 +1,12 @@
+package ch.post.education.apps.rainman.Model;
+
+public class Wind {
+
+    private double speed;
+    private int deg;
+
+    public Wind(double speed, int deg) {
+        this.speed = speed;
+        this.deg = deg;
+    }
+}
