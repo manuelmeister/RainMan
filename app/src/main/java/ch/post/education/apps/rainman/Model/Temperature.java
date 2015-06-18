3bdc0a119768ddf9cbf6264fe897940b08527e69
bmeism <news.manuelsworld@gmail.com>
2015-06-18 17:19:25 +0200

    Added temperature

---
+++ app/src/main/java/ch/post/education/apps/rainman/Model/Temperature.java
@@ -0,0 +1,47 @@
+package ch.post.education.apps.rainman.Model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Temperature {
+
+    private double day;
+    private double min;
+    private double max;
+    private double night;
+    private double evening;
+    private double morning;
+
+    public Temperature(JSONObject object) throws JSONException{
+        day = object.getDouble("day");
+        min = object.getDouble("min");
+        max = object.getDouble("max");
+        night = object.getDouble("night");
+        evening = object.getDouble("eve");
+        morning = object.getDouble("morning");
+    }
+
+    public double getDay() {
+        return day;
+    }
+
+    public double getMin() {
+        return min;
+    }
+
+    public double getMax() {
+        return max;
+    }
+
+    public double getNight() {
+        return night;
+    }
+
+    public double getEvening() {
+        return evening;
+    }
+
+    public double getMorning() {
+        return morning;
+    }
+}
