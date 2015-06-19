cedc63d58314d1d42b0214c5f2328f1c56562ce2
bmeism <news.manuelsworld@gmail.com>
2015-06-19 15:01:03 +0200

    Added animation

--- app/src/main/java/ch/post/education/apps/rainman/MainActivity.java
+++ app/src/main/java/ch/post/education/apps/rainman/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -23,7 +27,7 @@ public class MainActivity extends BasicActivity {
         setContentView(R.layout.activity_main);
 
         JSONAsyncTask task = new JSONAsyncTask(this);
-        task.execute("http://api.openweathermap.org/data/2.5/forecast/daily?q=Alaghsas&mode=json&units=metric&cnt=2");
+        task.execute("http://api.openweathermap.org/data/2.5/forecast/daily?q=thun&mode=json&units=metric&cnt=2");
     }
 
     @Override
@@ -59,7 +63,8 @@ public class MainActivity extends BasicActivity {
 
             FrameLayout bar_rain = (FrameLayout) findViewById(R.id.bar_rain);
             ViewGroup.LayoutParams bar_rain_layout = bar_rain.getLayoutParams();
-            bar_rain_layout.height = setHeight(forecast.getRain() * 5);
+            int bar_rain_height = getHeight(forecast.getRain() * 5);
+            expand(bar_rain,bar_rain_height);
 
             TextView bar_rain_value = (TextView) findViewById(R.id.bar_rain_value);
             if(forecast.getRain() != 0){
@@ -70,14 +75,17 @@ public class MainActivity extends BasicActivity {
 
             FrameLayout bar_pressure = (FrameLayout) findViewById(R.id.bar_pressure);
             ViewGroup.LayoutParams bar_pressure_layout = bar_pressure.getLayoutParams();
-            bar_pressure_layout.height = setHeight(forecast.getPressure()/6);
+            int bar_pressure_height = getHeight(forecast.getPressure() / 6);
+            expand(bar_pressure,bar_pressure_height);
 
             TextView bar_pressure_value = (TextView) findViewById(R.id.bar_pressure_value);
             bar_pressure_value.setText(String.valueOf(forecast.getPressure()) + " hPa");
 
             FrameLayout bar_temperature = (FrameLayout) findViewById(R.id.bar_temperature);
             ViewGroup.LayoutParams bar_temperature_layout = bar_temperature.getLayoutParams();
-            bar_temperature_layout.height = setHeight(forecast.getTemperature().getDay()*10);
+            int bar_temperature_height = getHeight(forecast.getTemperature().getDay() * 10);
+            expand(bar_temperature,bar_temperature_height);
+
             bar_temperature.setBackgroundColor(setBGColor(forecast.getTemperature().getDay()));
 
             TextView bar_temperature_value = (TextView) findViewById(R.id.bar_temperature_value);
@@ -88,7 +96,7 @@ public class MainActivity extends BasicActivity {
         }
     }
 
-    public int setHeight(double height){
+    public int getHeight(double height){
         float pixels = (float)((height >= 60) ? height : 60);
         return (int)(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, pixels, getResources().getDisplayMetrics()));
     }
@@ -109,5 +117,27 @@ public class MainActivity extends BasicActivity {
             color = R.color.temperature_background_freezing;
         }
         return getResources().getColor(color);
+    }
+
+    public static void expand(final View v, final int targetHeight) {
+        Animation a = new Animation()
+        {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = (int)(targetHeight * interpolatedTime);
+                v.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int)(targetHeight / v.getContext().getResources().getDisplayMetrics().density)*3);
+        a.setInterpolator(new AccelerateDecelerateInterpolator());
+        v.startAnimation(a);
+        v.requestLayout();
     }
 }
