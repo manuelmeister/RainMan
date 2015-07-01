e882b031a0af5e7383587eaed3c89d25c642b044
bmeism <news.manuelsworld@gmail.com>
2015-07-01 08:48:19 +0200

    Added comments

--- app/src/main/java/ch/post/education/apps/rainman/BasicActivity.java
+++ app/src/main/java/ch/post/education/apps/rainman/BasicActivity.java
@@ -2,8 +2,13 @@ package ch.post.education.apps.rainman;
 
 import org.json.JSONObject;
 
-public  interface BasicActivity {
+public interface BasicActivity {
 
+    /**
+     * Gets the jsonObject from the {@link JSONAsyncTask} at {@link JSONAsyncTask#onPostExecute(JSONObject)} and processes the JSONObject and visualizes it
+     *
+     * @param jsonObject {@link JSONObject}
+     */
     void display(JSONObject jsonObject);
 
 }
